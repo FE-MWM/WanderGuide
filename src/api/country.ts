55666a6a6b23f6269328bb54d533d94babf49444
MoonSsunny <moonsun116@naver.com>
2024-05-07 22:38:00 +0900

    [19] fix : 타입스크립트 형 정리 및 코드 정리
    - try catch 문 수정
    -Array type 정리

--- src/api/country.ts
+++ src/api/country.ts
@@ -1,26 +1,32 @@
 import axios from "axios";
 
+type Data = {
+  "ISO alpha2": string;
+  "ISO alpha3": string;
+  "ISO numeric": number;
+  "대륙명_공통 대륙코드": string;
+  대륙명_행정표준코드: string;
+  "대륙명_외교부 직제": string;
+  영문명: string;
+  한글명: string;
+};
+
 type Country = {
   page: number;
   perPage: number;
   totalCount: number;
   currentCount: number;
   matchCount: number;
-  data: Array<{
-    "ISO alpha2": string;
-    "ISO alpha3": string;
-    "ISO numeric": number;
-    "대륙명_공통 대륙코드": string;
-    대륙명_행정표준코드: string;
-    "대륙명_외교부 직제": string;
-    영문명: string;
-    한글명: string;
-  }>;
+  data: Data[];
 };
 
-export const getCountries = async (): Promise<Country[]> => {
-  const response = await axios.get(
-    `https://api.odcloud.kr/api/15091117/v1/uddi:bbcc2939-88e0-4a54-af03-ab819b4130e6?perPage=196&serviceKey=${process.env.REACT_APP_API_COUNTRY}`
-  );
-  return response.data as Country[];
+export const getCountries = async <T = Country[]>(): Promise<T> => {
+  const API_URL = `https://api.odcloud.kr/api/15091117/v1/uddi:bbcc2939-88e0-4a54-af03-ab819b4130e6?perPage=196&serviceKey=${process.env.REACT_APP_API_COUNTRY}`;
+  try {
+    const response = await axios.get(API_URL);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw Error("나라 정보를 불러오는데 실패했습니다.");
+  }
 };
