ce527da0f44cf7a04ce26f423a0f926798098eb4
MoonSsunny <moonsun116@naver.com>
2024-05-08 12:14:09 +0900

    [10] feat: 공통 컴포넌트 로딩스피너 추가
    - 공통 컴포넌트로 로딩스피너를 추가하였습니다.

---
+++ src/components/common/IsLoading.jsx
@@ -0,0 +1,9 @@
+const IsLoading = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <div className="animate-spin rounded-full h-7 w-7 border-4 border-gray-300 border-t-transparent" />
+    </div>
+  );
+};
+
+export default IsLoading;
