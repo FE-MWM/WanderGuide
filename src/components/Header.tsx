896b6ac9c21fc153a846aeff4bd73e3e374c7d22
jsmsue010 <jsmsue010@gmail.com>
2024-05-12 17:57:00 +0900

    [48]feat:선택한 id로 indexedDB 데이터 조회

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,27 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getData } from "../indexeddb/indexedDB";
 
-const MainHeader = () => {
+type MainHeaderProps = {
+  selected: number;
+};
+
+const MainHeader = ({ selected }: MainHeaderProps) => {
+  // indexedDB 비동기여서. indexedDB를 기다리는 분기 추가
+  const [country, setCountry] = useState<string>("");
+  useEffect(() => {
+    getData("plan", selected + 1).then((res) => setCountry(res.title));
+  }, [selected]);
+
   return (
     <div className="bg-white h-[57px] border-b border-gray-200 flex items-center justify-between">
-      <div className="pl-2">
-        <span>도시이름</span>
-      </div>
-      <div className="flex items-center">
-        <button className="w-[34px] h-[34px] bg-blue-50 rounded-full flex items-center justify-center hover:bg-blue-100">
-          <img
-            className="w-[20px] h-[20px]"
-            src="/images/settings.svg"
-            alt="logo"
-          />
-        </button>
-        <button className="w-[34px] h-[34px] bg-blue-50 rounded-full flex items-center justify-center m-3 hover:bg-blue-100">
-          <img
-            className="w-[20px] h-[20px]"
-            src="/images/notification.svg"
-            alt="notification"
-          />
-        </button>
-      </div>
+      {country && (
+        <>
+          <div className="pl-2">
+            <span>{country}</span>
+          </div>
+          <div className="flex items-center">
+            <button className="w-[34px] h-[34px] bg-blue-50 rounded-full flex items-center justify-center hover:bg-blue-100">
+              <img
+                className="w-[20px] h-[20px]"
+                src="/images/settings.svg"
+                alt="logo"
+              />
+            </button>
+            <button className="w-[34px] h-[34px] bg-blue-50 rounded-full flex items-center justify-center m-3 hover:bg-blue-100">
+              <img
+                className="w-[20px] h-[20px]"
+                src="/images/notification.svg"
+                alt="notification"
+              />
+            </button>
+          </div>{" "}
+        </>
+      )}
     </div>
   );
 };
---
+++ src/indexeddb/indexedDB.ts
@@ -0,0 +1,151 @@
+import { Item } from "./Indexed";
+
+let request: IDBOpenDBRequest;
+let db: IDBDatabase;
+let version = 1;
+
+export const initDB = (): Promise<boolean | IDBDatabase> => {
+  return new Promise((resolve) => {
+    const request = indexedDB.open("myDB", 1);
+
+    // if the data object store doesn't exist, create it
+    request.onupgradeneeded = (ev) => {
+      const db = request.result;
+
+      const oldVersion = ev.oldVersion;
+      console.log("od", oldVersion);
+      if (oldVersion < 1) {
+        db.createObjectStore(`plan`, {
+          keyPath: "id",
+          autoIncrement: true
+        });
+      }
+      // no need to resolve here
+    };
+
+    request.onsuccess = (e) => {
+      db = request.result;
+      // get current version and store it
+      version = db.version;
+      resolve(request.result);
+    };
+
+    request.onerror = (e) => {
+      resolve(false);
+    };
+  });
+};
+
+export const addData = <T>(
+  storeName: string,
+  data: T
+): Promise<T | string | null> => {
+  return new Promise((resolve) => {
+    request = indexedDB.open("myDB", version);
+
+    request.onsuccess = () => {
+      console.log("request.onsuccess - addData", data);
+      db = request.result;
+      const tx = db.transaction(storeName, "readwrite");
+      const store = tx.objectStore(storeName);
+      store.add(data);
+      resolve(data);
+    };
+
+    request.onerror = () => {
+      const error = request.error?.message;
+      if (error) {
+        resolve(error);
+      } else {
+        resolve("Unknown error");
+      }
+    };
+  });
+};
+
+export const deleteData = (
+  storeName: string,
+  key: string
+): Promise<boolean> => {
+  return new Promise((resolve) => {
+    request = indexedDB.open("myDB", version);
+
+    request.onsuccess = () => {
+      console.log("request.onsuccess - deleteData", key);
+      db = request.result;
+      const tx = db.transaction(storeName, "readwrite");
+      const store = tx.objectStore(storeName);
+      const res = store.delete(key);
+      res.onsuccess = () => {
+        resolve(true);
+      };
+      res.onerror = () => {
+        resolve(false);
+      };
+    };
+  });
+};
+
+export const updateData = <T>(
+  storeName: string,
+  key: number,
+  data: T
+): Promise<T | string | null> => {
+  return new Promise((resolve) => {
+    request = indexedDB.open("myDB", version);
+
+    request.onsuccess = () => {
+      console.log("request.onsuccess - updateData", key);
+      db = request.result;
+      const tx = db.transaction(storeName, "readwrite");
+      const store = tx.objectStore(storeName);
+      const res = store.get(key);
+      res.onsuccess = () => {
+        const newData = { ...res.result, ...data };
+        store.put(newData);
+        resolve(newData);
+      };
+      res.onerror = () => {
+        resolve(null);
+      };
+    };
+  });
+};
+
+export const getStoreData = <T>(storeName: string): Promise<Item[]> => {
+  return new Promise((resolve) => {
+    const request = indexedDB.open("myDB");
+
+    request.onsuccess = () => {
+      console.log("request.onsuccess - getAllData");
+      const db = request.result;
+      const tx = db.transaction(storeName, "readonly");
+      const store = tx.objectStore(storeName);
+      const res = store.getAll();
+      res.onsuccess = () => {
+        console.log(res.result);
+        resolve(res.result);
+      };
+    };
+  });
+};
+
+export const getData = (storeName: string, id: number): Promise<Item> => {
+  return new Promise((resolve) => {
+    const request = indexedDB.open("myDB");
+
+    request.onsuccess = () => {
+      console.log("request.onsuccess - getAllData");
+      const db = request.result;
+      const tx = db.transaction(storeName, "readonly");
+      const store = tx.objectStore(storeName);
+      const res = store.get(id);
+      res.onsuccess = () => {
+        console.log(res.result);
+        resolve(res.result);
+      };
+    };
+  });
+};
+
+export {};
