a8918a8b7b8d4738ffe76b972e8a85c3137176df
moonseonju <sjmoon@glowmee.com>
2024-05-13 01:10:13 +0900

    [56] feat :  백그라운드 클릭시 모달창 꺼지도록 설정

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -2,15 +2,25 @@ import React from "react";
 import { useModal } from "../../context/ModalContext";
 
 const Modal = () => {
-  const { isOpen, title, content } = useModal();
+  const { isOpen, title, content, closeModal } = useModal();
+
+  const stopPropagation = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    event.stopPropagation();
+  };
   return (
     <>
       {isOpen && (
         <div
           id="default-modal"
           className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full md:inset-0 max-h-full bg-gray-800 bg-opacity-50"
+          onClick={() => closeModal()}
         >
-          <div className="relative p-4 w-full max-w-2xl max-h-full">
+          <div
+            className="relative p-4 w-full max-w-2xl max-h-full"
+            onClick={stopPropagation}
+          >
             <div className="relative bg-white rounded-lg shadow">
               <div className="flex items-center px-6 py-5 border-b">
                 <h3 className="text-xl font-semibold">{title}</h3>
