3b2357bd79b88c8825246adc4e559e258a9998c8
jsmsue010 <jsmsue010@gmail.com>
2024-05-12 20:14:28 +0900

    [46]fix: 불필요한 프레그먼트 제거

--- src/components/CountriesSelectBox.tsx
+++ src/components/CountriesSelectBox.tsx
@@ -8,30 +8,28 @@ const CountriesSelectBox = () => {
   const dataRef = useRef<Data | undefined>();
 
   return (
-    <>
-      <SelectBox>
-        <SelectBox.input
-          title="WHERE"
-          titleStyle="text-modal1"
-          debounceFunc={(str) => search(str)}
-        >
-          <SelectBox.list bgColor="bg-white" layout="h-[150px] overflow-y-auto">
-            {value?.map((ele, idx) => {
-              return (
-                <SelectBox.item
-                  key={idx}
-                  value={`${ele["ISO numeric"]}`}
-                  label={ele.한글명}
-                  onClick={() => (dataRef.current = ele)}
-                >
-                  {ele.한글명}
-                </SelectBox.item>
-              );
-            })}
-          </SelectBox.list>
-        </SelectBox.input>
-      </SelectBox>
-    </>
+    <SelectBox>
+      <SelectBox.input
+        title="WHERE"
+        titleStyle="text-modal1"
+        debounceFunc={(str) => search(str)}
+      >
+        <SelectBox.list bgColor="bg-white" layout="h-[150px] overflow-y-auto">
+          {value?.map((ele, idx) => {
+            return (
+              <SelectBox.item
+                key={idx}
+                value={`${ele["ISO numeric"]}`}
+                label={ele.한글명}
+                onClick={() => (dataRef.current = ele)}
+              >
+                {ele.한글명}
+              </SelectBox.item>
+            );
+          })}
+        </SelectBox.list>
+      </SelectBox.input>
+    </SelectBox>
   );
 };
 
