4a35a6de6c8715db00f59e16c76c992d0e525f62
moonseonju <sjmoon@glowmee.com>
2024-05-12 16:08:30 +0900

    [44] feat: tab 상태값 context로 생성 후, 변경, 더보기 클릭시 탭 이동하도록 수정

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import "./App.css";
 import Home from "./page/Home";
 import { ModalProvider } from "./context/ModalContext";
 import Modal from "./components/common/Modal";
+import { TabProvider } from "./context/TabContext";
 
 function App() {
   return (
-    <ModalProvider>
-      <Home />
-      <Modal />
-    </ModalProvider>
+    <TabProvider>
+      <ModalProvider>
+        <Home />
+        <Modal />
+      </ModalProvider>
+    </TabProvider>
   );
 }
 
