683b6c05a7aac5d5f12cade1a92b242cd8029a2d
moonseonju <sjmoon@glowmee.com>
2024-05-14 00:04:08 +0900

    [68] feat : 버튼으로 수정

--- src/components/common/NoWriteData.tsx
+++ src/components/common/NoWriteData.tsx
@@ -4,11 +4,13 @@ const NoWriteData = ({ title }: PropsData) => {
   return (
     <div className="flex flex-col justify-center items-center h-full gap-3">
       <p className="text-sm text-cool-gray">{title} 추가하기</p>
-      <img
-        src="/images/add-button.svg"
-        className="w-[20px] h-[20px]"
-        alt="plus"
-      />
+      <button>
+        <img
+          src="/images/add-button.svg"
+          className="w-[20px] h-[20px]"
+          alt="plus"
+        />
+      </button>
     </div>
   );
 };
