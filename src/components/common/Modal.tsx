d22b474296d694dc1e612f6ae53fb024db9ad1e5
MoonSsunny <moonsun116@naver.com>
2024-05-16 01:34:06 +0900

    [101] feat : 모달 높이 수정

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -14,7 +14,7 @@ const Modal = () => {
       {isOpen && (
         <div
           id="default-modal"
-          className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full md:inset-0 max-h-full bg-gray-800 bg-opacity-50"
+          className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full md:inset-0 h-full bg-gray-800 bg-opacity-50"
           onClick={() => closeModal()}
         >
           <div
