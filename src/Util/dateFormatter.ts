64c821e5da04ea61171129b9e0c08e4a3f99183d
MoonSsunny <moonsun116@naver.com>
2024-05-15 21:40:46 +0900

    [86] feat : 날짜 조정 함수 추가

--- src/Util/dateFormatter.ts
+++ src/Util/dateFormatter.ts
@@ -1,4 +1,5 @@
 import dayjs from "dayjs";
+import "dayjs/locale/ko";
 
 export const formatYear = (date: number): string => {
   return dayjs.unix(date).format("YYYY-MM-DD");
@@ -16,4 +17,12 @@ export const formatDate = (date?: number): string => {
 
 export const formatUtc = (date?: number | string): number => {
   return date ? dayjs(date).valueOf() : dayjs().valueOf();
+};
+
+export const formatDay = (date: string): string => {
+  return dayjs(date).locale("ko").format("ddd");
+};
+
+export const formatMonthDay = (date: string): string => {
+  return dayjs(date).format("MM/DD");
 };
