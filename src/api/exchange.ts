32624a974c64b7c61c46fbc9e04692bd634744aa
jsmsue010 <jsmsue010@gmail.com>
2024-05-16 14:24:59 +0900

    [105] fix: 크롬 익스텐션 환율 api cors 해결

--- src/api/exchange.ts
+++ src/api/exchange.ts
@@ -6,7 +6,7 @@ export const getExchangeList = async () => {
   const checkWeekend = getFridayIfWeekend(today).format("YYYY-MM-DD");
   try {
     const res = axios.get(
-      `/site/program/financial/exchangeJSON?authkey=${process.env.REACT_APP_API_EXCHANGE}&searchdate=${checkWeekend}&data=AP01`
+      `https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=${process.env.REACT_APP_API_EXCHANGE}&searchdate=${checkWeekend}&data=AP01`
       //`/site/program/financial/exchangeJSON?authkey=YWVgIoxXxKTI3HNUAZYsfsrV9XTB0WIf&searchdate=20240423&data=AP01`
     );
     const req = await res;
