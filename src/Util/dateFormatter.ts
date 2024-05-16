b3f519701c902e9c1609d5ddc38a5a402d5b8af1
MoonSsunny <moonsun116@naver.com>
2024-05-16 21:06:18 +0900

    [111] feat :  유틸함수로 분리

--- src/Util/dateFormatter.ts
+++ src/Util/dateFormatter.ts
@@ -1,5 +1,12 @@
 import dayjs from "dayjs";
 import "dayjs/locale/ko";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+import duration from "dayjs/plugin/duration";
+import { getCountry } from "countries-and-timezones";
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.extend(duration);
 
 export const formatYear = (date: number): string => {
   return dayjs.unix(date).format("YYYY-MM-DD");
@@ -25,4 +32,26 @@ export const formatDay = (date: string): string => {
 
 export const formatMonthDay = (date: string): string => {
   return dayjs(date).format("MM/DD");
+};
+
+export const fetchTimeForCountry = async (countryCode: string) => {
+  const countryInfo = getCountry(countryCode);
+  if (!countryInfo) {
+    return;
+  }
+  const timezone = countryInfo.timezones[0];
+  const currentTime = dayjs().tz(timezone).format("YYYY-MM-DD HH:mm:ss");
+  return currentTime;
+};
+
+export const calculateTimeDifference = async (time1: string, time2: string) => {
+  const format = "HH:mm:ss";
+
+  const moment1 = dayjs(time1, format);
+  const moment2 = dayjs(time2, format);
+
+  const timeDiff = moment1.diff(moment2, "minutes");
+
+  const diffDuration = dayjs.duration(timeDiff, "minutes").format("HH");
+  return diffDuration;
 };
